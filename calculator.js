af903306f99772d940529b0a22c0ed1b0f41ad37
Takafumi Mashiko <tmashiko7@gmail.com>
2023-05-24 12:26:48 -0400

    added css

--- calculator.js
+++ calculator.js
@@ -13,3 +13,22 @@ function multiply (a,b) {
 function divide (a,b) {
     return a/b;
 }
+
+const firstVariable = document.querySelector('.firstVariable');
+const operatorSymbol = document.querySelector('.operator');
+const secondVariable = document.querySelector('.secondVariable');
+
+function operate(firstVariable, operatorSymbol, secondVariable) {
+    if(operatorSymbol === '+') {
+        add(firstVariable, secondVariable);
+    }
+    else if(operatorSymbol === '-'){
+        subtract(firstVariable, secondVariable);
+    }
+    else if(operatorSymbol === '*'){
+        multiply(firstVariable, secondVariable);
+    }
+    else if(operatorSymbol === '/'){
+        divide(firstVariable, secondVariable);
+    }
+}
